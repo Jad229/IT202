61dd855adfea015fad63b2f36f3ce59ddd94a176
Jad229 <jad229@njit.edu>
2022-10-02 23:10:24 -0400

    Week4 coding assignment fixed alert functionality

--- Week4/script.js
+++ Week4/script.js
@@ -1,19 +1,20 @@
-function isValidUcid(ucidInput){
+function isUcidValid() {
+    let ucidInput = document.getElementById("ucid").value;
     let ucidList = ["jad229", "paa22", "zap2001", "Sis404"];
 
     let re = /[a-zA-Z]+\d*/i;
 
-    if(re.test(ucidInput)){
-        ucidList.forEach(function(ucid){
-            if(ucid === ucidInput){
-                alert("VALID UCID FORMAT AND UCID FOUND");
-            }
-            else{
-                alert("VALID UCID FORMAT BUT INVALID UCID");
-            }
-        });
+    if (re.test(ucidInput)) {
+        if(ucidList.indexOf(ucidInput) > -1){
+            alert("VALID UCID FORMAT AND UCID FOUND")
+        }
+        else{
+            alert("VALID UCID FORMAT BUT INVALID UCID");
+        }
     }
-    else{
+    else {
         alert("UCID DOES NOT CONFORM TO VALID FORMAT");
     }
 }
+
+
