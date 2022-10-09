e7681cf76e6f5349b606add63e04e0b589724142
Jad229 <jad229@njit.edu>
2022-10-09 19:06:09 -0400

    week 5 coding assignment complete

---
+++ Week5/script.js
@@ -0,0 +1,11 @@
+let iceCreamElement = document.getElementById("ice-cream");
+let coneElement = document.getElementById("cone");
+
+iceCreamElement.classList.toggle("toggle");
+
+function toggleImage(){
+    iceCreamElement.classList.toggle("toggle");
+    coneElement.classList.toggle("toggle");
+}
+
+
